e4d6d8606781293a9586a77e4c1c66863291c60e
yanshiqwq <yanshiqwq@outlook.com>
2024-03-24 02:25:16 +0800

    modified:   server.js

--- server.js
+++ server.js
@@ -47,81 +47,50 @@ async function cmdLoop() {
 
 const child_process = require("child_process");
 const chalk = require("chalk");
-const version = `v${child_process
-	.execSync("git tag", { encoding: "utf8" })
-	.trim()}-${child_process
-	.execSync('git log --oneline -n 1 --format="%h"', { encoding: "utf8" })
-	.trim()}`;
+const version = `v${child_process.execSync("git tag", { encoding: "utf8" }).trim()}-${child_process.execSync('git log --oneline -n 1 --format="%h"', { encoding: "utf8" }).trim()}`;
 
 const cmdApi = require("./cmd");
 const battleManager = require("./api/battle");
 const dataManager = require("./api/data");
-new Promise(async function (resolve, reject) {
-	var parseConfig = function () {
-		return new Promise(async function (resolve, reject) {
-			try {
-				var configFile = await yaml.parse(
-					fs.readFileSync(`${__dirname}/config.yml`, "utf-8")
-				);
-				resolve(configFile);
-			} catch (err) {
-				reject(new Error(`Failed to parse config file: ${err.stack}`));
-			}
-		});
-	};
-	var parseLang = function (configFile) {
-		return new Promise(function (resolve, reject) {
-			try {
-				var lang = configFile.server.lang;
-				var langFile = yaml.parse(
-					fs.readFileSync(`${__dirname}/lang.yml`, "utf-8")
-				);
-				if (lang in langFile) {
-					resolve(langFile[configFile.server.lang]);
-				} else {
-					reject(new Error(`Invalid language: ${lang}`));
-				}
-			} catch (err) {
-				reject(
-					new Error(`Failed to parse language file: ${err.stack}`)
-				);
-			}
-		});
-	};
+
+var parseConfig = function () {
 	try {
-		var configFile = await parseConfig();
-		var langFile = await parseLang(configFile);
-		resolve([configFile, langFile]);
+		var configFile = yaml.parse(fs.readFileSync(`${__dirname}/config.yml`, "utf-8"));
+		return configFile;
 	} catch (err) {
-		reject(err);
+		throw new Error(`Failed to parse config file: ${err.stack}`);
 	}
-})
-	.then((files) => {
-		global.config = files[0];
-		global.lang = files[1];
+};
+var parseLang = function (configFile) {
+	try {
+		var lang = configFile.server.lang;
+		var langFile = yaml.parse(fs.readFileSync(`${__dirname}/lang.yml`, "utf-8"));
+		if (lang in langFile) {
+			return langFile[configFile.server.lang];
+		} else {
+			throw new Error(`Invalid language: ${lang}`);
+		}
+	} catch (err) {
+		throw new Error(`Failed to parse language file: ${err.stack}`);
+	}
+};
+global.config = parseConfig();
+global.lang = parseLang(config);
 
-		app.use(express.json());
-		app.use(function (err, next) {
-			error(err.message);
-			next(err);
-		});
-
-		app.use("./api/battle", battleManager);
-		app.use("./api/data", dataManager);
-
-		app.listen(config.server.port, config.server.host, async () => {
-			console.info(
-				chalk.bold.blueBright(lang.server.motd.render(version))
-			);
-			info(
-				lang.server.serverStart.render(
-					config.server.host,
-					config.server.port
-				)
-			);
-			cmdLoop();
-		});
-	})
-	.catch((err) => {
-		error(lang.mainLoopError.render(err));
+try {
+	app.use(express.json());
+	app.use(function (err, next) {
+		error(err.message);
+		next(err);
 	});
+	app.use("./api/battle", battleManager);
+	app.use("./api/data", dataManager);
+
+	app.listen(config.server.port, config.server.host, async () => {
+		console.info(chalk.bold.blueBright(lang.server.motd.render(version)));
+		info(lang.server.serverStart.render(config.server.host, config.server.port));
+		cmdLoop();
+	});
+} catch (err) {
+	error(lang.mainLoopError.render(err));
+};
